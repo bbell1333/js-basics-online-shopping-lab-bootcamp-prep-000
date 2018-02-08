7c9957a3cc837f1f28173e36f44f9dc9365ac5eb
Bryan Bell <bbell1333@gmail.com>
2018-02-08 14:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === ""){
+  if (cardNumber === null){
     return console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
